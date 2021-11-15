3d2a457eb2ff215a46b305253c3f7b644722152a
Carter Abdallah <carterabdallah@gmail.com>
2021-11-15 14:42:20 -0500

    Changed app.py and Added Bootstrap Styling

--- app.py
+++ app.py
@@ -1,10 +1,34 @@
-from flask import Flask
+from flask import Flask, jsonify, abort, request, make_response, url_for, session
+from flask import render_template, redirect
+app = Flask(__name__, static_url_path="")
 
-app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+@app.route('/')
+def hello():
+    return render_template('index.html')
+
+
+@app.errorhandler(400)
+def bad_request(error):
+    """ 400 page route.
+
+    get:
+        description: Endpoint to return a bad request 400 page.
+        responses: Returns 400 object.
+    """
+    return make_response(jsonify({'error': 'Bad request'}), 400)
+
+
+
+@app.errorhandler(404)
+def not_found(error):
+    """ 404 page route.
+
+    get:
+        description: Endpoint to return a not found 404 page.
+        responses: Returns 404 object.
+    """
+    return make_response(jsonify({'error': 'Not found'}), 404)
 
 
 if __name__ == '__main__':
