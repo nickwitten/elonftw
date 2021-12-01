05608da22aaa879081ac0039bf737db894fb1cad
Carter Abdallah <carterabdallah@gmail.com>
2021-11-30 20:28:49 -0500

    Changes to App.py and Index + JS to show stock price at closing

--- stockstream.py
+++ stockstream.py
@@ -1,39 +1,40 @@
 import yfinance as yf
 
-tsla = yf.Ticker("TSLA")
-data = yf.download(  # or pdr.get_data_yahoo(...
-    # tickers list or string as well
-    tickers="TSLA",
 
-    # use "period" instead of start/end
-    # valid periods: 1d,5d,1mo,3mo,6mo,1y,2y,5y,10y,ytd,max
-    # (optional, default is '1mo')
-    period="7d",
+def getCloseData():
+    tsla = yf.Ticker("TSLA")
+    data = yf.download(  # or pdr.get_data_yahoo(...
+        # tickers list or string as well
+        tickers="TSLA",
 
-    # fetch data by interval (including intraday if period < 60 days)
-    # valid intervals: 1m,2m,5m,15m,30m,60m,90m,1h,1d,5d,1wk,1mo,3mo
-    # (optional, default is '1d')
-    interval="1m",
+        # use "period" instead of start/end
+        # valid periods: 1d,5d,1mo,3mo,6mo,1y,2y,5y,10y,ytd,max
+        # (optional, default is '1mo')
+        period="7d",
 
-    # group by ticker (to access via data['SPY'])
-    # (optional, default is 'column')
-    group_by='ticker',
+        # fetch data by interval (including intraday if period < 60 days)
+        # valid intervals: 1m,2m,5m,15m,30m,60m,90m,1h,1d,5d,1wk,1mo,3mo
+        # (optional, default is '1d')
+        interval="1h",
 
-    # adjust all OHLC automatically
-    # (optional, default is False)
-    auto_adjust=True,
+        # group by ticker (to access via data['SPY'])
+        # (optional, default is 'column')
+        group_by='ticker',
 
-    # download pre/post regular market hours data
-    # (optional, default is False)
-    prepost=False,
+        # adjust all OHLC automatically
+        # (optional, default is False)
+        auto_adjust=True,
 
-    # use threads for mass downloading? (True/False/Integer)
-    # (optional, default is True)
-    threads=True,
+        # download pre/post regular market hours data
+        # (optional, default is False)
+        prepost=False,
 
-    # proxy URL scheme use use when downloading?
-    # (optional, default is None)
-    proxy=None
-)
+        # use threads for mass downloading? (True/False/Integer)
+        # (optional, default is True)
+        threads=True,
 
-print(data)
+        # proxy URL scheme use use when downloading?
+        # (optional, default is None)
+        proxy=None
+    )
+    return data['Close']
